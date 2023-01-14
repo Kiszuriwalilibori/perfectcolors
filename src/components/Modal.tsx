276c518951138f9759c8544b67f61a5310313b60
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2023-01-14 16:26:28 +0100

    Modeal and NoPage styling refactored for more DRY

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,20 +10,7 @@ import useDispatchAction from "hooks/useDispatchAction";
 import ModalDataItem from "./ModalItem";
 
 import { getSelectedColorModalData, getIsModalVisible } from "reduxware/selectors";
-
-const style = {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    pt: 2,
-    px: 4,
-    pb: 3,
-};
+import { boxStyle } from "styles";
 
 function MyModal() {
     const isVisible = useSelector(getIsModalVisible, shallowEqual);
@@ -34,7 +21,7 @@ function MyModal() {
 
     return (
         <Modal open={isVisible} onClose={() => hideModal()} aria-labelledby="parent-modal-title">
-            <Box sx={{ ...style, width: 300 }}>
+            <Box sx={{ ...boxStyle }}>
                 <h2 id="parent-modal-title">Color details</h2>
                 <Stack direction="column" divider={<Divider orientation="horizontal" flexItem />} spacing={1}>
                     {data.map(item => {
